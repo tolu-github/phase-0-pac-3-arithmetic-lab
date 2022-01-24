b9ad76e492317209dbca9a9cd932087a4b5ecaf6
tolu <toluokupe@yahoo.com>
2022-01-24 00:13:12 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function divide(a,b){
+    return a/b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function increment(n) {
+    return n+1;
+}
+function decrement(n) {
+    return n-1;
+}
+parseInt("2.2222", 10);
+parseInt("nonsense!", 10);
+
+function makeInt(a, base){
+    return parseInt(a, 10);
+}
+function preserveDecimal(a){
+    return parseFloat(a);
+}
